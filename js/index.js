a6cad4583255fbd3427d67415dff5bb05302b10a
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-04-17 16:30:46 -0300

    att

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-alert('Um alerta rodando de uma página externa.')
+document.getElementById("texto").innerHTML='Meu primeiro Texto <b>JS<b>!';
+alert("Tem que colocar aspas para texto");
+console.log('Oi isso é um console.log')
+
+var a =1;
+var b=5;
+var c =a + b; 
+console.log(b-a)
+
+var leo = "leandro";
+var LEO = 'LEANDRO';
+console.log(leo)
+console.log(LEO)
